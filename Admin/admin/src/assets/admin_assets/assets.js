41876cf39a1bf944f9c9c88ceb375f332e0661d5
Shresth941 <1rn20ec087.shresthsingh@gmail.com>
2024-09-17 07:23:39 +0530

    Resolved  conflicts

--- Admin/admin/src/assets/admin_assets/assets.js
+++ Admin/admin/src/assets/admin_assets/assets.js
@@ -14,8 +14,3 @@ export const assets ={
     parcel_icon
 }
 
-<<<<<<< HEAD
-export const url = 'https://food-app-back-eh9z.onrender.com'
-=======
-export const url = 'https://food-app-back-eh9z.onrender.com'
->>>>>>> aa4c94d0d717d438ceff4b8cb692a0c5747df069
